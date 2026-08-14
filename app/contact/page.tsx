11ee1da4922da75d068ebdd93be9776243542a20
Tyler Kinney <tylerkinney50@gmail.com>
2026-08-13 20:54:43 -0500

    chore: change the contact button from the image to the email text

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,7 +14,7 @@ const ContactPage = () => {
         { name: 'Megan Anderson', role: 'Team Electrical Captain', email: 'safety@team4198.org', image: '/contact/Megan.png' },
         { name: 'Varian & Lucian', role: 'Outreach Lead', email: 'outreach@team4198.org', image: '/contact/generic-profile-picture.jpeg' },
         { name: 'Marcus Bauer', role: 'Media Lead', email: 'media@team4198.org', image: '/contact/generic-profile-picture.jpeg' },
-        { name: 'Eric Johnson', role: 'Safety Lead', email: 'ericjohnson@isd110.org', image: '/contact/generic-profile-picture.jpeg' }
+        { name: 'Eric Johnson', role: 'Safety Lead', email: 'ericjohnson@isd110.org', image: '/contact/generic-profile-picture.jpeg' },
     ]
 
     return (
@@ -24,12 +24,12 @@ const ContactPage = () => {
             <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mb-8'>
                 {members.map((member, index) => (
                     <div key={index} className='w-full h-full max-w-[26rem] flex flex-row bg-purple-950/5 backdrop-blur-md border border-white/10 rounded-xl p-6'>
-                        <a href={`mailto:${member.email}`} className='my-auto'><Image src={member.image} alt={member.name} width={120} height={120} draggable={false} className='object-cover max-h-[120px] rounded-2xl' /></a>
+                        <Image src={member.image} alt={member.name} width={120} height={120} draggable={false} className='object-cover max-h-[120px] rounded-2xl' />
 
                         <div className='flex flex-col justify-center ml-4'>
                             <h2 className='text-2xl text-gray-200 mb-2'>{member.name}</h2>
                             <p className='text-main'>{member.role}</p>
-                            <p className='text-main text-[clamp(0.8rem,1.5vw,1rem)] whitespace-nowrap overflow-hidden text-ellipsis'>{member.email}</p>
+                            <a href={`mailto:${member.email}`} className='text-main text-[clamp(0.8rem,1.5vw,1rem)] whitespace-nowrap overflow-hidden text-ellipsis'>{member.email}</a>
                         </div>
                     </div>
                 ))}
